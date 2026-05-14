6cdeb436301567d66c29528901dfe252a49f1a6d
RobotsAreCrazy <RobotsAreCrazy@users.noreply.github.com>
2026-05-14 04:08:51 +0100

    Change repo branch to 'twrp-11' and clean old attempts
    Updated repository branch to 'twrp-11' and added cleanup step.
    
    Signed-off-by: RobotsAreCrazy <RobotsAreCrazy@users.noreply.github.com>

--- orangefox_sync.sh
+++ orangefox_sync.sh
@@ -2,11 +2,11 @@
 mkdir -p ~/fox/source
 cd ~/fox/source
 
-# We are switching to 'v3.6' branch which is the stable base for Android 12
-repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-12.1 --depth=1
-# If that still fails, the script will automatically try the alternate branch name
-if [ $? -ne 0 ]; then
-  repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b v3.6 --depth=1
-fi
+# Wiping old failed attempts from 1000025373.jpg
+rm -rf .repo
 
-repo sync -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+# Using the branch that is verified to exist on this mirror
+repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-11 --depth=1
+
+# Run the sync
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
