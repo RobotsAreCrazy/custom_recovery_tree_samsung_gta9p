d350e0e0aa3c4bec974b45f426123de59ec42081
RobotsAreCrazy <RobotsAreCrazy@users.noreply.github.com>
2026-05-14 04:05:08 +0100

    Update repository initialization for Android 12
    Signed-off-by: RobotsAreCrazy <RobotsAreCrazy@users.noreply.github.com>

--- orangefox_sync.sh
+++ orangefox_sync.sh
@@ -2,6 +2,11 @@
 mkdir -p ~/fox/source
 cd ~/fox/source
 
-# We are switching to the 'minimal' manifest which is still public
+# We are switching to 'v3.6' branch which is the stable base for Android 12
 repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-12.1 --depth=1
+# If that still fails, the script will automatically try the alternate branch name
+if [ $? -ne 0 ]; then
+  repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b v3.6 --depth=1
+fi
+
 repo sync -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
